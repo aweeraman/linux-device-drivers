dd41229407ced2b5c68f93ea9d4a573850efc524
Anuradha Weeraman <anuradha@weeraman.com>
2018-01-02 06:19:51 +0530

    Trapped for signals

--- simple_kthread.c
+++ simple_kthread.c
@@ -1,6 +1,7 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 #include <linux/delay.h>
+#include <linux/sched/signal.h>
 
 MODULE_LICENSE("GPL");
 
@@ -8,9 +9,18 @@ static struct task_struct *kthread_struct;
 
 static int thread_fn(void *unused) 
 {
+  allow_signal(SIGKILL);
+
   while(!kthread_should_stop()) {
     printk(KERN_INFO "Simple thread is running");
     ssleep(5);
+
+    // Unlike in user space, signals should be manually
+    // checked for, and actioned upon
+    if (signal_pending(kthread_struct)) {
+      printk(KERN_INFO "Killing simple thread - SIGKILL received");
+      break;
+    }
   }
 
   printk(KERN_INFO "Simple thread is stopping");
